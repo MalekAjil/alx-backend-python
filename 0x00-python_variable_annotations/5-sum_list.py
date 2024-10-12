65aade8ff9e62eb5c43030cc2dd6fd49471335c2
MalekAjil <aboanasajil@gmail.com>
2024-10-12 00:03:35 +0000

    task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Sum List Module"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns sum of list items"""
     s: float = 0
     for x in input_list:
