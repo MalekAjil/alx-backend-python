4ab0ab79d764fe1dfcb228ecaf244a85cdc25699
MalekAjil <aboanasajil@gmail.com>
2024-11-02 11:50:18 -0700

    task0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""test_utils module"""
+from unittest import TestCase
+from utils import access_nested_map
+
+
+class TestAccessNestedMap (TestCase):
+    """TestAccessNestedMap class"""
+
