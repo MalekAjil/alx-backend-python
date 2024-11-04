089d21b153d0e2cf7422fc6a50aff279474f62bd
MalekAjil <aboanasajil@gmail.com>
2024-11-04 21:09:57 +0000

    task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python3
 """test_utils module"""
-from unittest import TestCase
-from utils import access_nested_map, get_json, memoize
+import unittest
+from parameterized import parameterized
+import utils
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 
 
-class TestAccessNestedMap (TestCase):
+class TestAccessNestedMap (unittest.TestCase):
     """TestAccessNestedMap class"""
-    @parameterized.expand
-    def test_access_nested_map():
-        """test access_nested_map method"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2) ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, ex: Any) -> None:
+        """test Access nested map.
+        Parameters
+        ----------
+        nested_map: Mapping
+        A nested map
+        path: Sequence
+        a sequence of key representing a path to the value
+        ex:
+        expected
+        """
+        self.assertEqual(access_nested_map(nested_map, path), ex)
 
     @parameterized.expand
-    def test_access_nested_map_exception():
+    def test_access_nested_map_exception(self):
         """test_access_nested_map_exception mothod"""
         assertRaises KeyError
 
 
 class TestGetJson(unittest.TestCase):
     """TestGetJson class"""
-    def test_get_json():
+    def test_get_json(self):
         """test get_json method"""
         assert get_json == test_payload
 
 
 class TestMemoize(unittest.TestCase):
     """TestMemoize class"""
-    def test_memoize():
+    def test_memoize(self):
         """test memoize method"""
