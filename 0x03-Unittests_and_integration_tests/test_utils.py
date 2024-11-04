aa19d0953afd6576d3654c0afac80446d8ededa2
MalekAjil <aboanasajil@gmail.com>
2024-11-04 21:12:41 +0000

    task0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,8 +17,10 @@ class TestAccessNestedMap (unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2) ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, ex: Any) -> None:
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
+                               ex: Any) -> None:
         """test Access nested map.
         Parameters
         ----------
